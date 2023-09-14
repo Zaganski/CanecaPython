4649fb968a85f29217a8965c984e4b4d87785db2
Math <mathserpa@gmail.com>
2023-09-14 14:01:48 -0300

    Create CanecaImpressão.py

---
+++ CanecaImpressão.py
@@ -0,0 +1,16 @@
+while True:
+    coffee = 'coffee situation.'
+    if coffee == 'empty':
+        print ('fill')
+        break
+    elif coffee == 'full':
+        print('drink')
+        break
+    else:
+        print("it's ok")
+    pass
+print ('So proceed ... ')    
+
+
+#teste de impressão
+        
